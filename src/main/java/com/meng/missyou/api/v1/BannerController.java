cc9918813ebbb57f6298e2b58ed73429c4079d53
Jintaimeng <929357244@qq.com>
2020-03-05 17:09:38 +0800

    数据传输对象DTO的接收

--- src/main/java/com/meng/missyou/api/v1/BannerController.java
+++ src/main/java/com/meng/missyou/api/v1/BannerController.java
@@ -1,5 +1,6 @@
 package com.meng.missyou.api.v1;
 
+import com.meng.missyou.dto.PersonDTO;
 import com.meng.missyou.exception.http.ForbiddenException;
 import com.meng.missyou.exception.http.NotFoundException;
 import com.meng.missyou.sample.IConnect;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Controller
 /*若在类上加上@ResponseBody ，则不需要在每个方法上写@ResponseBody
@@ -48,9 +50,9 @@ public class BannerController {
 //    }
     //@RequestMapping(value = "/test",method = {RequestMethod.GET,
     // RequestMethod.POST})  RequestMapping比GetMapping功能更强大
-    @GetMapping("/test/{id}")//路由  RESTFulAPI
+    @PostMapping("/test/{id}")//路由  RESTFulAPI
     @ResponseBody
-    public String test(@PathVariable Integer id,@RequestParam String name) {
+    public String test(@PathVariable Integer id, @RequestParam String name, @RequestBody PersonDTO person) {
         iSkill.r();//此处就不用实例化了
         throw new ForbiddenException(10000);
         //throw new Exception("这里错了");

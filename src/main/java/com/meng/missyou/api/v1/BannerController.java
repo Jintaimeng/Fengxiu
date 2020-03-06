4700b90635438e2733490396e3e56cde51219b6c
Jintaimeng <929357244@qq.com>
2020-03-06 17:05:33 +0800

    使用@Validated注解进行基础参数校验

--- src/main/java/com/meng/missyou/api/v1/BannerController.java
+++ src/main/java/com/meng/missyou/api/v1/BannerController.java
@@ -4,16 +4,19 @@ import com.meng.missyou.dto.PersonDTO;
 import com.meng.missyou.sample.ISkill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Max;
 
 @Controller
 /*若在类上加上@ResponseBody ，则不需要在每个方法上写@ResponseBody
-* @Controller和@ResponseBody可以用@RestController代替，
-* @RestController包括前面两者*/
+ * @Controller和@ResponseBody可以用@RestController代替，
+ * @RestController包括前面两者*/
 
 /*若url前缀都一样，可在类上使用@RequestMapping("/v1/banner")*/
 //@RequestMapping("/banner")
-
+@Validated //使参数校验生效
 public class BannerController {
     @Autowired //将Diana注入进来  @Autowired(required = false)说明允许为空值
     //@Qualifier("irelia")//强制注入irelia
@@ -39,17 +42,18 @@ public class BannerController {
 //        response.getWriter().write("Hello,TaiMeng");
 //        //return "Hello,TaiMeng";
 //    }
-    //@RequestMapping(value = "/test",method = {RequestMethod.GET,
-    // RequestMethod.POST})  RequestMapping比GetMapping功能更强大
-    @PostMapping("/test/{id}")//路由  RESTFulAPI
-    @ResponseBody
-    public PersonDTO test(@PathVariable Integer id, @RequestParam String name, @RequestBody PersonDTO person) {
-        iSkill.r();//此处就不用实例化了
-        PersonDTO dto = PersonDTO.builder().name("taimeng").age(18).build();
-        //throw new ForbiddenException(10000);
-        //throw new Exception("这里错了");
-        return dto;
-    }
+//@RequestMapping(value = "/test",method = {RequestMethod.GET,
+// RequestMethod.POST})  RequestMapping比GetMapping功能更强大
+@PostMapping("/test/{id}") //路由  RESTFulAPI
+@ResponseBody
+
+public PersonDTO test(@PathVariable @Max(value = 10, message = "不可以超过10") Integer id, @RequestParam String name, @RequestBody PersonDTO person) {
+    iSkill.r();//此处就不用实例化了
+    PersonDTO dto = PersonDTO.builder().name("taimeng").age(18).build();
+    //throw new ForbiddenException(10000);
+    //throw new Exception("这里错了");
+    return dto;
+}
 
 //    @GetMapping("/test1")
 //    @ResponseBody

421074bb712e2f9756d8f0fbec4fe0cd29417500
Jintaimeng <929357244@qq.com>
2020-03-08 20:47:25 +0800

    修改数据库名称，未成功

--- src/main/java/com/meng/missyou/dto/PersonDTO.java
+++ src/main/java/com/meng/missyou/dto/PersonDTO.java
@@ -2,20 +2,21 @@ package com.meng.missyou.dto;
 
 
 import com.meng.missyou.validators.PasswordEqual;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
 //@Setter
 //@AllArgsConstructor//全参构造
 //@NoArgsConstructor//无参构造
-//@RequiredArgsConstructor//部分参数的构造函数
+@RequiredArgsConstructor//部分参数的构造函数  与@NonNull一起使用，谁不能为空谁为参数
 //@Data 不止包括Getter和Setter方法，还包括equals、hashCode、toString等
-@Builder
+//@Builder
 @PasswordEqual(min = 1, message = "两次密码不一样")
 public class PersonDTO {
-    //@NonNull//不能传入空值
+    @NonNull//不能传入空值
     @Length(min = 5, max = 12, message = "长度错误")
     private String name;
     private Integer age;
--- src/main/java/com/meng/missyou/model/Banner.java
+++ src/main/java/com/meng/missyou/model/Banner.java
@@ -1,13 +1,15 @@
 package com.meng.missyou.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "bannerone") //使表名与类名不相同，自定义表名
 public class Banner {
     @Id
     private long id;
+    @Column(length = 16)
     private String name;
+    @Transient //使表中没有description
     private String description;
     private String img;
     private String title;

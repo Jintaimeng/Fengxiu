2a34c6e662aebc4a72de87bf19d3a85cb16b7f25
Jintaimeng <929357244@qq.com>
2020-03-11 17:13:46 +0800

    JPA的Repository定义

---
+++ src/main/java/com/meng/missyou/repository/BannerRepository.java
@@ -0,0 +1,12 @@
+package com.meng.missyou.repository;
+
+import com.meng.missyou.model.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+    Banner findOneById(Long id);
+
+    Banner findOneByName(String name);
+}

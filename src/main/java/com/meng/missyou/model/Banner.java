6eb3b25047be4b022c6ab17dd6027ec29bf06d97
Jintaimeng <929357244@qq.com>
2020-03-11 16:24:06 +0800

    添加外键，id自增长

--- src/main/java/com/meng/missyou/model/Banner.java
+++ src/main/java/com/meng/missyou/model/Banner.java
@@ -7,6 +7,7 @@ import java.util.List;
 //@Table(name = "banner") //使表名与类名不相同，自定义表名
 public class Banner {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(length = 16)
@@ -17,5 +18,6 @@ public class Banner {
     private String img;
     private String title;
     @OneToMany
+    @JoinColumn(name = "bannerId")
     private List<BannerItem> items;
 }
--- src/main/java/com/meng/missyou/model/BannerItem.java
+++ src/main/java/com/meng/missyou/model/BannerItem.java
@@ -1,14 +1,19 @@
 package com.meng.missyou.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class BannerItem {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String img;
     private String keyword;
     private short type;
     private String name;
+
+    private long bannerId;
 }

143f298d01005eefc98f60d362b6cc924791f7c9
Jintaimeng <929357244@qq.com>
2020-03-17 17:20:00 +0800

    @MappedSuperclass

--- src/main/java/com/meng/missyou/model/BaseEntity.java
+++ src/main/java/com/meng/missyou/model/BaseEntity.java
@@ -3,11 +3,14 @@ package com.meng.missyou.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseEntity {
+
     private Date createTime;
     private Date updateTime;
     private Date deleteTime;

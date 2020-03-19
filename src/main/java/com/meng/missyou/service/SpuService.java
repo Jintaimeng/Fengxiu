d26e7e0e54350ee48440d5419ca2fc76dbf127a9
Jintaimeng <929357244@qq.com>
2020-03-19 13:18:22 +0800

    查询列表类数据

--- src/main/java/com/meng/missyou/service/SpuService.java
+++ src/main/java/com/meng/missyou/service/SpuService.java
@@ -5,6 +5,8 @@ import com.meng.missyou.repository.SpuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SpuService {
     @Autowired
@@ -12,5 +14,9 @@ public class SpuService {
 
     public Spu getSpu(Long id) {
         return this.spuRepository.getOneById(id);
+    }
+
+    public List<Spu> getLatestPagingSpu() {
+        return this.spuRepository.findAll();
     }
 }

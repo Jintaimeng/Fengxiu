c6a7cbbea8fb3a92aebe3771c2a8824edcb84006
Jintaimeng <929357244@qq.com>
2020-03-20 12:24:11 +0800

    DozerBeanMapper拷贝属性

--- src/main/java/com/meng/missyou/api/v1/SpuController.java
+++ src/main/java/com/meng/missyou/api/v1/SpuController.java
@@ -1,8 +1,12 @@
 package com.meng.missyou.api.v1;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import com.meng.missyou.exception.http.NotFoundException;
 import com.meng.missyou.model.Spu;
 import com.meng.missyou.service.SpuService;
+import com.meng.missyou.vo.SpuSimplifyVO;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Positive;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,9 +34,24 @@ public class SpuController {
         return spu;
     }
 
+    @GetMapping("id/{id}/simplify")
+    public SpuSimplifyVO getSimplifySpu(@PathVariable @Positive Long id) {
+        Spu spu = this.spuService.getSpu(id);
+        SpuSimplifyVO vo = new SpuSimplifyVO();
+        BeanUtils.copyProperties(spu, vo);
+        return vo;
+    }
+
     @GetMapping("/latest")
-    public List<Spu> getLatestSpuList() {
-        return this.spuService.getLatestPagingSpu();
+    public List<SpuSimplifyVO> getLatestSpuList() {
+        List<Spu> spuList = this.spuService.getLatestPagingSpu();
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+        List<SpuSimplifyVO> vos = new ArrayList<>();
+        spuList.forEach(s -> {
+            SpuSimplifyVO vo = mapper.map(s, SpuSimplifyVO.class);
+            vos.add(vo);
+        });
+        return vos;
     }
 
 }
--- src/main/java/com/meng/missyou/vo/SpuSimplifyVO.java
+++ src/main/java/com/meng/missyou/vo/SpuSimplifyVO.java
@@ -1,5 +1,10 @@
 package com.meng.missyou.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SpuSimplifyVO {
     private Long id;
     private String title;

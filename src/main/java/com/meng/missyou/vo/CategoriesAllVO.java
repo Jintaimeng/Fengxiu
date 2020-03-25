89955117d86630b4d1e2430917b52d2e1a25a0bd
Jintaimeng <929357244@qq.com>
2020-03-25 15:07:27 +0800

    Java的Stream与Method Reference应用

--- src/main/java/com/meng/missyou/vo/CategoriesAllVO.java
+++ src/main/java/com/meng/missyou/vo/CategoriesAllVO.java
@@ -5,10 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class CategoriesAllVO {
-    private List<Category> roots;
-    private List<Category> subs;
+    private List<CategoryPureVO> roots;
+    private List<CategoryPureVO> subs;
+
+    public CategoriesAllVO(Map<Integer, List<Category>> map) {
+        //this.roots = map.get(1).stream().map(r->{return new CategoryPureVO(r);}).collect(Collectors.toList());
+        this.roots = map.get(1).stream().map(CategoryPureVO::new).collect(Collectors.toList());
+        this.subs = map.get(2).stream().map(CategoryPureVO::new).collect(Collectors.toList());
+    }
 }

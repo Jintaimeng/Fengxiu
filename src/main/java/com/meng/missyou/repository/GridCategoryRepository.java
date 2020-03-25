98eaff6775af87ea63a5fcb831a5c8f961800117
Jintaimeng <929357244@qq.com>
2020-03-25 16:18:50 +0800

    GridCategory

---
+++ src/main/java/com/meng/missyou/repository/GridCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.meng.missyou.repository;
+
+import com.meng.missyou.model.GridCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GridCategoryRepository extends JpaRepository<GridCategory, Long> {
+
+}

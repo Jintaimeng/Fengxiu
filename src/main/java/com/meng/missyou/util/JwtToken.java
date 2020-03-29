54b449b3e81158eb7cc0c9f641108a13ada8ba9c
Jintaimeng <929357244@qq.com>
2020-03-29 20:14:18 +0800

    Auth0生成JWT令牌

--- src/main/java/com/meng/missyou/util/JwtToken.java
+++ src/main/java/com/meng/missyou/util/JwtToken.java
@@ -1,4 +1,52 @@
 package com.meng.missyou.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class JwtToken {
+    private static String jwtKey;
+    private static Integer expiredTimeIn;
+
+    public static String makeToken(Long uid, Integer scope) {
+        return JwtToken.getToken(uid, scope);
+    }
+
+    private static String getToken(Long uid, Integer scope) {
+        //JWT的库： jjwt  Auth0
+        Algorithm algorithm = Algorithm.HMAC256(JwtToken.jwtKey);
+        Map<String, Date> map = JwtToken.calculateExpiredIssues();
+        String token = JWT.create().withClaim("uid", uid)
+                .withClaim("scope", scope)
+                .withExpiresAt(map.get("expiredTime")) //过期时间
+                .withIssuedAt(map.get("now"))          //签发时间
+                .sign(algorithm);
+        return token;
+    }
+
+    private static Map<String, Date> calculateExpiredIssues() {
+        Map<String, Date> map = new HashMap<>();
+        Calendar calendar = Calendar.getInstance();
+        Date now = calendar.getTime();
+        calendar.add(Calendar.SECOND, JwtToken.expiredTimeIn);
+        map.put("now", now);
+        map.put("expiredTime", calendar.getTime());
+        return map;
+    }
+
+    @Value("${missyou.security.jwt-key}")
+    public void setJwtKey(String jwtKey) {
+        JwtToken.jwtKey = jwtKey;
+    }
+
+    @Value("${missyou.security.token-expired-in}")
+    public void setExpiredTimeIn(Integer expiredTimeIn) {
+        JwtToken.expiredTimeIn = expiredTimeIn;
+    }
 }

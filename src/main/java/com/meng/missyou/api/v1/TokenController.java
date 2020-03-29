46f8863ec0fa79e8b50cd8507767cf9652b8543f
Jintaimeng <929357244@qq.com>
2020-03-29 15:34:52 +0800

    获取用户OpenId

--- src/main/java/com/meng/missyou/api/v1/TokenController.java
+++ src/main/java/com/meng/missyou/api/v1/TokenController.java
@@ -2,6 +2,8 @@ package com.meng.missyou.api.v1;
 
 import com.meng.missyou.dto.TokenGetDTO;
 import com.meng.missyou.exception.http.NotFoundException;
+import com.meng.missyou.service.WxAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +16,16 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "token")
 public class TokenController {
+    @Autowired
+    private WxAuthenticationService wxAuthenticationService;
+
     @PostMapping("")
     public Map<String, String> getToken(@RequestBody @Validated TokenGetDTO userData) {
         Map<String, String> map = new HashMap<>();
         String token = null;
         switch (userData.getType()) {
             case USER_WX:
+                wxAuthenticationService.code2Session(userData.getAccount());
                 break;
             case USER_Email:
                 break;
--- src/main/java/com/meng/missyou/service/WxAuthenticationService.java
+++ src/main/java/com/meng/missyou/service/WxAuthenticationService.java
@@ -1,11 +1,36 @@
 package com.meng.missyou.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.MessageFormat;
+import java.util.Map;
 
 @Service
 public class WxAuthenticationService {
-    public void code2Session(String code) {
+    @Autowired
+    private ObjectMapper mapper;
+    @Value("${wx.code2session}")
+    private String code2SessionUrl;
+    @Value("${wx.appid}")
+    private String appid;
+    @Value("${wx.appsecret}")
+    private String appsecret;
 
+    public String code2Session(String code) {
+        String url = MessageFormat.format(this.code2SessionUrl, this.appid, this.appsecret, code);
+        RestTemplate restTemplate = new RestTemplate();
+        String sessionText = restTemplate.getForObject(url, String.class);
+        try {
+            Map<String, Object> session = mapper.readValue(sessionText, Map.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }

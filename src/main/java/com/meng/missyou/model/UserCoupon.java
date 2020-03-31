e77f241d8611ad282090f2df31c51a494edb8330
Jintaimeng <929357244@qq.com>
2020-03-31 15:58:00 +0800

    UserCoupon

--- src/main/java/com/meng/missyou/model/UserCoupon.java
+++ src/main/java/com/meng/missyou/model/UserCoupon.java
@@ -1,106 +1,26 @@
 package com.meng.missyou.model;
 
-import javax.persistence.*;
-import java.sql.Timestamp;
-import java.util.Objects;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "user_coupon", schema = "sleeve", catalog = "")
 public class UserCoupon {
-    private Object id;
-    private Object userId;
-    private Object couponId;
-    private Object status;
-    private Timestamp createTime;
-    private Object orderId;
-    private Timestamp updateTime;
-
     @Id
-    @Column(name = "id")
-    public Object getId() {
-        return id;
-    }
-
-    public void setId(Object id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "user_id")
-    public Object getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Object userId) {
-        this.userId = userId;
-    }
-
-    @Basic
-    @Column(name = "coupon_id")
-    public Object getCouponId() {
-        return couponId;
-    }
-
-    public void setCouponId(Object couponId) {
-        this.couponId = couponId;
-    }
-
-    @Basic
-    @Column(name = "status")
-    public Object getStatus() {
-        return status;
-    }
-
-    public void setStatus(Object status) {
-        this.status = status;
-    }
-
-    @Basic
-    @Column(name = "create_time")
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
-    @Basic
-    @Column(name = "order_id")
-    public Object getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Object orderId) {
-        this.orderId = orderId;
-    }
-
-    @Basic
-    @Column(name = "update_time")
-    public Timestamp getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Timestamp updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserCoupon that = (UserCoupon) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(userId, that.userId) &&
-                Objects.equals(couponId, that.couponId) &&
-                Objects.equals(status, that.status) &&
-                Objects.equals(createTime, that.createTime) &&
-                Objects.equals(orderId, that.orderId) &&
-                Objects.equals(updateTime, that.updateTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, userId, couponId, status, createTime, orderId, updateTime);
-    }
+    private Long id;
+    private Long userId;
+    private Long couponId;
+    private Integer status;
+    private Date createTime;
+    private Long orderId;
+    //private Timestamp updateTime;
 }

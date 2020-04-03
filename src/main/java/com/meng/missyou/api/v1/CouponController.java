55a1565399c0beb58ab9e0d5572aaeb56c427b37
Jintaimeng <929357244@qq.com>
2020-04-03 23:18:51 +0800

    获取我可用的优惠券（带分类数据）

--- src/main/java/com/meng/missyou/api/v1/CouponController.java
+++ src/main/java/com/meng/missyou/api/v1/CouponController.java
@@ -4,6 +4,7 @@ import com.meng.missyou.core.Interceptors.ScopeLevel;
 import com.meng.missyou.core.LocalUser;
 import com.meng.missyou.core.enumeration.CouponStatus;
 import com.meng.missyou.exception.CreateSuccess;
+import com.meng.missyou.exception.http.ParameterException;
 import com.meng.missyou.model.Coupon;
 import com.meng.missyou.service.CouponService;
 import com.meng.missyou.vo.CouponPureVO;
@@ -52,11 +53,16 @@ public class CouponController {
         List<Coupon> couponList;
         switch (CouponStatus.toType(status)) {
             case AVAILABLE:
+                couponList = this.couponService.getMyAvailableCoupons(uid);
                 break;
             case USED:
+                couponList = this.couponService.getMyUsedCoupons(uid);
                 break;
             case EXPIRED:
+                couponList = this.couponService.getMyExpiredCoupons(uid);
                 break;
+            default:
+                throw new ParameterException(40001);
         }
         return CouponPureVO.getList(couponList);
     }
--- src/main/java/com/meng/missyou/service/CouponService.java
+++ src/main/java/com/meng/missyou/service/CouponService.java
@@ -57,14 +57,17 @@ public class CouponService {
     }
 
     public List<Coupon> getMyAvailableCoupons(Long uid) {
-
+        Date now = new Date();
+        return this.couponRepository.findMyAvailable(uid, now);
     }
 
     public List<Coupon> getMyUsedCoupons(Long uid) {
-
+        Date now = new Date();
+        return this.couponRepository.findMyUsed(uid, now);
     }
 
     public List<Coupon> getMyExpiredCoupons(Long uid) {
-
+        Date now = new Date();
+        return this.couponRepository.findMyExpired(uid, now);
     }
 }

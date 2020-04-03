d0e33b7194f85095d2454a50d3bb95d3db20dbd1
Jintaimeng <929357244@qq.com>
2020-04-03 15:55:08 +0800

    CouponStatus

---
+++ src/main/java/com/meng/missyou/core/enumeration/CouponStatus.java
@@ -0,0 +1,13 @@
+package com.meng.missyou.core.enumeration;
+
+public enum CouponStatus {
+    AVAILABLE(1, "可以使用，未过期"),
+    USED(2, "已使用"),
+    EXPIRED(3, "未使用，已过期");
+
+    private Integer value;
+
+    CouponStatus(Integer value, String description) {
+        this.value = value;
+    }
+}

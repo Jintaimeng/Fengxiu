2111709ffbba8c2f99e6adabff6ec785e9410db6
Jintaimeng <929357244@qq.com>
2020-04-07 14:23:41 +0800

    校验CouponChecker

--- src/main/java/com/meng/missyou/logic/OrderChecker.java
+++ src/main/java/com/meng/missyou/logic/OrderChecker.java
@@ -1,21 +1,27 @@
 package com.meng.missyou.logic;
 
+import com.meng.missyou.bo.SkuOrderBO;
 import com.meng.missyou.dto.OrderDTO;
 import com.meng.missyou.dto.SkuInfoDTO;
 import com.meng.missyou.exception.http.ParameterException;
 import com.meng.missyou.model.Sku;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderChecker {
     private OrderDTO orderDTO;
     private List<Sku> serverSkuList;
     private CouponChecker couponChecker;
+    private Integer maxSkuLimit;
 
-    public OrderChecker(OrderDTO orderDTO, List<Sku> serverSkuList, CouponChecker couponChecker) {
+
+    public OrderChecker(OrderDTO orderDTO, List<Sku> serverSkuList, CouponChecker couponChecker, Integer maxSkuLimit) {
         this.orderDTO = orderDTO;
         this.serverSkuList = serverSkuList;
         this.couponChecker = couponChecker;
+        this.maxSkuLimit = maxSkuLimit;
     }
 
     public void isOk() {
@@ -25,13 +31,36 @@ public class OrderChecker {
         //购买数量是否大于库存量
         //sku购买上限
         //优惠券校验
+        BigDecimal serverTotalPrice = new BigDecimal("0");
+        List<SkuOrderBO> skuOrderBOList = new ArrayList<>();
         this.skuNotOnSale(this.serverSkuList.size(), this.orderDTO.getSkuInfoDTOList().size());
         for (int i = 0; i < this.serverSkuList.size(); i++) {
             Sku sku = this.serverSkuList.get(i);
             SkuInfoDTO skuInfoDTO = this.orderDTO.getSkuInfoDTOList().get(i);
             this.containsSoldOutSku(sku);
             this.beyondSkuStock(sku, skuInfoDTO);
+            serverTotalPrice.add(this.calculateSkuOrderPrice(sku, skuInfoDTO));
+            skuOrderBOList.add(new SkuOrderBO(sku, skuInfoDTO));
         }
+        this.totalPriceIsOk(orderDTO.getTotalPrice(), serverTotalPrice);
+        if (this.couponChecker != null) {
+            this.couponChecker.isOk();
+            this.couponChecker.canBeUsed(skuOrderBOList, serverTotalPrice);
+            this.couponChecker.finalTotalPriceIsOk(orderDTO.getFinalTotalPrice(), serverTotalPrice);
+        }
+    }
+
+    private void totalPriceIsOk(BigDecimal orderTotalPrice, BigDecimal serverTotalPrice) {
+        if (orderTotalPrice.compareTo(serverTotalPrice) != 0) {
+            throw new ParameterException(50005);
+        }
+    }
+
+    private BigDecimal calculateSkuOrderPrice(Sku sku, SkuInfoDTO skuInfoDTO) {
+        if (skuInfoDTO.getCount() <= 0) {
+            throw new ParameterException(50007);
+        }
+        return sku.getActualPrice().multiply(new BigDecimal(skuInfoDTO.getCount()));
     }
 
     private void skuNotOnSale(int count1, int count2) {

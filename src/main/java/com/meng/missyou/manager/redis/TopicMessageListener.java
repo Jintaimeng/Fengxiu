27c717b738d0099916b4df35675400f56492d2b4
Jintaimeng <929357244@qq.com>
2020-04-14 11:53:30 +0800

    调用订单取消方法

--- src/main/java/com/meng/missyou/manager/redis/TopicMessageListener.java
+++ src/main/java/com/meng/missyou/manager/redis/TopicMessageListener.java
@@ -1,16 +1,22 @@
 package com.meng.missyou.manager.redis;
 
+import com.meng.missyou.bo.OrderMessageBO;
+import com.meng.missyou.service.OrderCancelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 
 public class TopicMessageListener implements MessageListener {
+    @Autowired
+    private OrderCancelService orderCancelService;
+
     @Override
-    public void onMessage(Message message, byte[] pattern) {
+    public void onMessage(Message message, byte[] bytes) {
         byte[] body = message.getBody();
         byte[] channel = message.getChannel();
         String expiredKey = new String(body);
         String topic = new String(channel);
-        System.out.println(expiredKey);
-        System.out.println(topic);
+        OrderMessageBO orderMessageBO = new OrderMessageBO(expiredKey);
+        this.orderCancelService.cancel(orderMessageBO);
     }
 }
